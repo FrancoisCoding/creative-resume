442cb91ded970d3d7043639f8520975da6335d09
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-06 01:49:30 -0400

    Styled side one in body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <span>About 5,240,000 results (0.42 seconds)</span>
+      <span className="results">About 5,240,000 results (0.42 seconds)</span>
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+export default function Body() {
+  return (
+    <div className="bodySection">
+      <div className="bodySideOne">
+        <section>
+          <div className="bodyLink">
+            <h3 className="body-blue-title">
+              About Fragle | Isaiah Francois's website
+            </h3>
+            <p className="body-green-links">www.fragle.com.com/about</p>
+          </div>
+          <p className="body-description">
+            View the profiles of people named Isaiah Francois. Join Facebook to
+            connect with Isaiah Francois and others you may know. Facebook gives
+            people the power...
+          </p>
+        </section>
+        <section>
+          <div className="bodyLink">
+            <h3 className="body-blue-title">
+              Isaiah Francois Profiles | Facebook
+            </h3>
+            <p className="body-green-links">
+              https://www.facebook.com/public/Isaiah-Francois
+            </p>
+          </div>
+          <p className="body-description">
+            View the profiles of people named Isaiah Francois. Join Facebook to
+            connect with Isaiah Francois and others you may know. Facebook gives
+            people the power...
+          </p>
+        </section>
+        <section>
+          <div className="bodyLink">
+            <h3 className="body-blue-title">
+              Isaiah Francois Profiles | Facebook
+            </h3>
+            <p className="body-green-links">
+              https://www.facebook.com/public/Isaiah-Francois
+            </p>
+          </div>
+          <p className="body-description">
+            View the profiles of people named Isaiah Francois. Join Facebook to
+            connect with Isaiah Francois and others you may know. Facebook gives
+            people the power...
+          </p>
+        </section>
+        <section>
+          <div className="bodyLink">
+            <h3 className="body-blue-title">
+              Isaiah Francois Profiles | Facebook
+            </h3>
+            <p className="body-green-links">
+              https://www.facebook.com/public/Isaiah-Francois
+            </p>
+          </div>
+          <p className="body-description">
+            View the profiles of people named Isaiah Francois. Join Facebook to
+            connect with Isaiah Francois and others you may know. Facebook gives
+            people the power...
+          </p>
+        </section>
+      </div>
+      <div className="bodySideTwo">
+        <section>
+          <h1>Google Account</h1>
+          <p>
+            Where you’ll find tools to secure and manage your Google Account and
+            control your data
+          </p>
+          <button>Go To Google Acccount</button>
+        </section>
+        <section>
+          <p>Contact Information</p>
+          <h1>Isaiah Francois</h1>
+          <button>Go To Google Acccount</button>
+        </section>
+      </div>
+    </div>
+  );
+}
