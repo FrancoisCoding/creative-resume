fafaa9183288b68045a58bcc2da5bd2a651fc2ea
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-06 13:19:32 -0400

    Added updates to link and body sides

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaPhone } from "react-icons/fa";
+import { FaMailBulk } from "react-icons/fa";
 
 export default function Body() {
   return (
@@ -12,65 +14,127 @@ export default function Body() {
             <p className="body-green-links">www.fragle.com.com/about</p>
           </div>
           <p className="body-description">
-            I'm constantly pushing myself to think differently and create
-            something new. Forget thinking outside the box, I want to make my
-            own shape ...
+            Thinking ahead is what i do best. Creating a resume can be a simple
+            task, but why not make it creative in the process ...
           </p>
         </section>
         <section>
           <div className="bodyLink">
-            <h3 className="body-blue-title">
-              Isaiah Francois - Lambda School - Orlando, Florida Area | LinkedIn
-            </h3>
+            <h3 className="body-blue-title">Personal Info | Isaiah Francois</h3>
+            <p className="body-green-links">www.learnaboutme.com/isaiah</p>
+          </div>
+          <p className="body-description">
+            <FaPhone /> (352)-989-3703 | <FaMailBulk /> francoiscoding@yahoo.com
+            | Portfolio : https://francois-isaiah.netlify.com/
+          </p>
+        </section>
+        <section>
+          <div className="bodyLink">
+            <a
+              href="https://lambdaschool.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3 className="body-blue-title">Education | Isaiah Francois</h3>
+            </a>
+            <p className="body-green-links">https://lambdaschool.com/</p>
+          </div>
+          <p className="body-description">
+            Attended Lambda School Full Stack Web Development program. Here I
+            learned HTML, CSS, JavaScript, React, Node, and Express.
+          </p>
+        </section>
+        <section>
+          <div className="bodyLink">
+            <a
+              href="https://github.com/FrancoisCoding"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3 className="body-blue-title">
+                Technologies/Languages | Isaiah Francois
+              </h3>
+            </a>
             <p className="body-green-links">
-              https://www.linkedin.com/in/isaiah-francois-56a5b4188/
+              https://github.com/FrancoisCoding
             </p>
           </div>
           <p className="body-description">
-            View Isaiah Francois' profile on LinkedIn, the world's largest
-            professional community. Isaiah's education is listed on their
-            profile. See the complete profile on ...
-          </p>
-        </section>
-        <section>
-          <div className="bodyLink">
-            <h3 className="body-blue-title">
-              Isaiah Francois (@FrancoisCoding) | Twitter
-            </h3>
-            <p className="body-green-links">
-              https://twitter.com/FrancoisCoding
-            </p>
-          </div>
-          <p className="body-description">
-            The latest Tweets from Isaiah Francois (@FrancoisCoding). 🧠
-            Developer | @lambdaschool | “Any fool can write code that a computer
-            can understand.
-          </p>
-        </section>
-        <section>
-          <div className="bodyLink">
-            <h3 className="body-blue-title">Isaiah Francois Resume</h3>
-            <p className="body-green-links">www.fragle.com/resume</p>
-          </div>
-          <p className="body-description">
-            Isaiah Francois's resume on fragle.com. See all related education
-            and work experience. References available upon request ...
+            HTML | CSS | JavaScript | Bootstrap | React | Python | Node |
+            Express | C# | Ruby | Unity Engine
           </p>
         </section>
       </div>
       <div className="bodySideTwo">
         <section>
-          <h1>Google Account</h1>
-          <p>
-            Where you’ll find tools to secure and manage your Google Account and
-            control your data
-          </p>
-          <button>Go To Google Acccount</button>
-        </section>
-        <section>
-          <p>Contact Information</p>
-          <h1>Isaiah Francois</h1>
-          <button>Go To Google Acccount</button>
+          <h1>Projects</h1>
+          <div>
+            <a
+              href="https://francois-game-center.netlify.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>Francois-Games Center</h3>
+            </a>
+            <ul>
+              <li className="bullets">
+                Created gaming platform where users can find latest news and
+                info on video games | Technologies Used: React, CSS, HTML,
+                JavaScript, Bootstrap
+              </li>
+            </ul>
+          </div>
+          <div>
+            <a
+              href="https://francoiscoding.itch.io/mathgame"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>Math Game</h3>
+            </a>
+            <ul>
+              <li className="bullets">
+                Created math game to help students test there skills with
+                multiplication, addition, and subtraction | Technologies Used:
+                CSS, HTML, JavaScript
+              </li>
+            </ul>
+          </div>
+          <div>
+            <a
+              href="https://play.google.com/store/apps/details?id=saneFreakyGaming.KnifeRunifeRun"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>Knife Run</h3>
+            </a>
+            <ul>
+              <li className="bullets">Created the music soundtrack </li>
+              <li className="bullets">
+                Made the game natively on pc and ported to Android |
+                Technologies Used: C#, Unity Engine
+              </li>
+            </ul>
+          </div>
+          <div>
+            <a
+              href="https://secret-recipes.netlify.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>Secret Family Recipe Cookbook</h3>
+            </a>
+            <ul>
+              <li className="bullets">Integrated Search functionality</li>
+              <li className="bullets">
+                Added ability to add recipes, and edit recipes
+              </li>
+              <li className="bullets">
+                Created pages with loading transition | Technologies Used:
+                React, CSS, HTML, JavaScript, Bootstrap
+              </li>
+            </ul>
+          </div>
         </section>
       </div>
     </div>
